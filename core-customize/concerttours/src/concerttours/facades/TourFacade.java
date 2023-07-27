3ff5f54074a4bc58f8bde779a39eaef41d5c61ef
jp-ag <jpaguinsky@gmail.com>
2023-07-27 10:42:02 -0300

    NICE

---
+++ core-customize/concerttours/src/concerttours/facades/TourFacade.java
@@ -0,0 +1,9 @@
+// Hybris123SnippetStart concerttours.facades.TourFacade.java
+package concerttours.facades;
+import concerttours.data.TourData;
+ 
+public interface TourFacade
+{
+    TourData getTourDetails(final String tourId);
+}
+//Hybris123SnippetEnd
